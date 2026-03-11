3d669413e856f95bee1b5067d5b5851be5c1df35
Abdulkadir-DevOps <abdulkadirhadrami89@gmail.com>
2026-03-11 09:17:53 +0300

    Update app.py

--- examples/first-docker-file/app.py
+++ examples/first-docker-file/app.py
@@ -1,20 +1,16 @@
-import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-HOST = "0.0.0.0"
-PORT = 5000
+host = "0.0.0.0"
+port = 5000
 
-name = os.getenv("NAME", "Abdulkadir Osman")
-
-class Handler(BaseHTTPRequestHandler):
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-type","text/plain")
         self.end_headers()
-        message = f"Hello {name}! Docker container is running."
-        self.wfile.write(message.encode())
+        self.wfile.write(b"Hello Abdulkadir! Docker container is running")
 
-if __name__ == "__main__":
-    print("Server started on port", PORT)
-    server = HTTPServer((HOST, PORT), Handler)
-    server.serve_forever()
+server = HTTPServer((host, port), MyHandler)
+
+print("Server running on port 5000...")
+server.serve_forever()
